f85a4d87e98766893f0e54f320951bc3d489be89
unknown <dhoouglas@hotmail.com>
2024-02-06 15:58:34 -0300

    Category-button components and list of category

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,24 @@
-import { Header } from "@/components/header"
-import {View, Text} from "react-native"
+import { CategoryButton } from "@/components/category-button";
+import { Header } from "@/components/header";
+import { View, FlatList } from "react-native";
+import { CATEGORIES } from "@/utils/data/products";
 
 
 export default function Home() {
     return (
         <View className="flex-1 pt-8">
             <Header title="Faça seu pedido" cartQuantityItems={3}/>
+
+            <FlatList 
+                data={CATEGORIES}
+                keyExtractor={(item) => item}
+                renderItem={({ item }) => <CategoryButton title={item} />}
+                horizontal
+                className="max-h-10 mt-5"
+                showsHorizontalScrollIndicator={false}
+                contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
+            />
+            
         </View>
     )
 }
