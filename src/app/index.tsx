04d3b9347ebd6dd030f0dfc674ed203606a0434e
unknown <dhoouglas@hotmail.com>
2024-02-06 16:13:36 -0300

    Modifying category status

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import { CategoryButton } from "@/components/category-button";
 import { Header } from "@/components/header";
 import { View, FlatList } from "react-native";
@@ -5,6 +7,12 @@ import { CATEGORIES } from "@/utils/data/products";
 
 
 export default function Home() {
+    const [category, setCategory] = useState(CATEGORIES[0]);
+
+    function handleCategorySelected(selectedCategory: string) {
+        setCategory(selectedCategory)
+    }
+
     return (
         <View className="flex-1 pt-8">
             <Header title="Faça seu pedido" cartQuantityItems={3}/>
@@ -12,7 +20,13 @@ export default function Home() {
             <FlatList 
                 data={CATEGORIES}
                 keyExtractor={(item) => item}
-                renderItem={({ item }) => <CategoryButton title={item} />}
+                renderItem={({ item }) => (
+                    <CategoryButton 
+                        title={item} 
+                        isSelected={item == category} 
+                        onPress={() => handleCategorySelected(item)}
+                    />
+                )}
                 horizontal
                 className="max-h-10 mt-5"
                 showsHorizontalScrollIndicator={false}
