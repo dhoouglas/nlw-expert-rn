e34d4656192f069554e67eb11ca18be0acfaa220
unknown <dhoouglas@hotmail.com>
2024-02-08 15:56:18 -0300

    Input components, and config Button send in cart

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -3,6 +3,11 @@ import { Product } from "@/components/product"
 import { View, Text, ScrollView } from "react-native";
 import { useCartStore } from "@/stores/cart-store";
 import { formatCurrency } from "@/utils/functions/format-currency";
+import { Input } from "@/components/input";
+import {KeyboardAwareScrollView} from "react-native-keyboard-aware-scroll-view"
+import { Button } from "@/components/button";
+import { Feather } from "@expo/vector-icons"
+import { LinkButton } from "@/components/link-button";
 
 export default function Cart() {
     const cartStore = useCartStore();
@@ -13,9 +18,11 @@ export default function Cart() {
         <View className="flex-1 pt-8">
             <Header title="Seu carrinho"/>
 
+            <KeyboardAwareScrollView>
             <ScrollView>
+                <View className="p-5 flex-1 ">
                 {cartStore.products.length > 0 ? (
-                    <View className="p-5 flex-1 ">
+                    <View className="border-b border-slate-700 ">
                         {
                             cartStore.products.map((product) => (
                                 <Product key={product.id} data={product} />
@@ -37,7 +44,25 @@ export default function Cart() {
                         {total}
                     </Text>
                 </View>
+
+                <Input 
+                    placeholder="Informe o endereço de entrega com rua, bairro, CEP, número e complemento"
+                    className="" 
+                />
+                </View>
             </ScrollView>
+            </KeyboardAwareScrollView>
+
+            <View className="p-5 gap-5">
+                <Button>
+                    <Button.Text>Enviar Pedido</Button.Text>
+                    <Button.Icon>
+                        <Feather name="arrow-right-circle" size={20} />
+                    </Button.Icon>
+                </Button>
+                
+                <LinkButton title="Voltar ao cardápio" href="/" />
+            </View>
         </View>
     )
 }
