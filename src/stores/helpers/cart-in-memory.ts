d90121392c9b2a985298b7e278ed6c35533f04c4
unknown <dhoouglas@hotmail.com>
2024-02-08 15:31:32 -0300

    Cart components

--- src/stores/helpers/cart-in-memory.ts
+++ src/stores/helpers/cart-in-memory.ts
@@ -2,14 +2,15 @@ import { ProductProps } from "@/utils/data/products";
 import { ProductCartProps } from "../cart-store";
 
 export function add(products: ProductCartProps[], newProduct: ProductProps) {
-    const existingProduct = products.find(({ id }) => newProduct.id === id);
+	const existingProduct = products.find(({ id }) => newProduct.id === id);
 
-    if (existingProduct) {
-        return products.map((product) => product.id === existingProduct.id
-        ? { ...product, quatity: product.quantity + 1}
-        : product
-    )
-    }
+	if (existingProduct) {
+		return products.map((product) =>
+			product.id === existingProduct.id
+				? { ...product, quantity: product.quantity + 1 }
+				: product
+		);
+	}
 
-    return [...products, { ... newProduct, quantity: 1 }]
+	return [...products, { ...newProduct, quantity: 1 }];
 }
