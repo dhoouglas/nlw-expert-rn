a4cc596cd4b88cf6d2beb7c97fede2b2cf130c97
unknown <dhoouglas@hotmail.com>
2024-02-08 16:24:35 -0300

    Persist data on device

--- src/stores/cart-store.ts
+++ src/stores/cart-store.ts
@@ -1,6 +1,10 @@
 import { ProductProps } from "@/utils/data/products"
 import { create } from "zustand"
+import AsyncStorageStatic from "@react-native-async-storage/async-storage"
+import { createJSONStorage, persist } from "zustand/middleware"
+
 import * as cartInMemory from "./helpers/cart-in-memory"
+import AsyncStorage from "@react-native-async-storage/async-storage"
 
 export type ProductCartProps = ProductProps & {
     quantity: number
@@ -12,7 +16,8 @@ type StateProps = {
     remove: (productId: string) => void;
 }
 
-export const useCartStore = create<StateProps>((set) => ({
+export const useCartStore = create(
+    persist<StateProps>((set) => ({
     products: [],
 
     add: (product: ProductProps) => set((state) => ({
@@ -22,4 +27,7 @@ export const useCartStore = create<StateProps>((set) => ({
     remove: (productId: string) => set((state) => ({
         products: cartInMemory.remove(state.products, productId),
     })),
+}), {
+    name: "nlw-expert:cart",
+    storage: createJSONStorage(() => AsyncStorage),
 }))
